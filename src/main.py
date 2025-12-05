f3102ec8bf01237b57ca57cdabb72d46f57fa92a
aidar <anged@mail.ru>
2025-12-05 16:45:21 +0300

    add services

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from src.views import main_info
+from src.services import analyze_cashback
 
 if __name__ == "__main__":
     date_time = "2020-05-20 12:12:12"
-    print(main_info(date_time))
+    # print(main_info(date_time))
+
+    result_services = analyze_cashback("../data/operations.xlsx", 2018, 5)
+    print(result_services)
